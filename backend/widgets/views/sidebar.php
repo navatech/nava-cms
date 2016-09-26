d40055c03b69b822385ddcec4e628898893251f1
thuchm <thuchm92@gmail.com>
2016-09-26 11:53:27 +0700

    sub-menu fix

--- backend/widgets/views/sidebar.php
+++ backend/widgets/views/sidebar.php
@@ -29,3 +29,34 @@ use yii\widgets\Menu;
 		<div class="clearfix"></div>
 	</div>
 </nav>
+
+<script>
+	$(document).on('click', '.sidebar-menu a', function(e) {
+		if ($(this).parent().children('.sub-menu') === false) {
+			return;
+		}
+		var el = $(this);
+		var parent = $(this).parent().parent();
+		var li = $(this).parent();
+		var sub = $(this).parent().children('.sub-menu');
+
+		if(li.hasClass("open active")){
+			el.children('.arrow').removeClass("open active");
+			sub.slideUp(200, function() {
+				li.removeClass("open active");
+			});
+
+		}else{
+			parent.children('li.open').children('.sub-menu').slideUp(200);
+			parent.children('li.open').children('a').children('.arrow').removeClass('open active');
+			parent.children('li.open').removeClass("open active");
+			el.children('.arrow').addClass("open active");
+			sub.slideDown(200, function() {
+				li.addClass("open active");
+
+			});
+		}
+		//e.preventDefault();
+	});
+
+</script>
